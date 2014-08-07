9cae86cae398e4a3d043eba592acd108c6d097e5
rcook <rcook@riotgames.com>
2014-08-07 04:31:11 -0700

    Cleaned directory

--- src/spine/view/ViewManager.js
+++
@@ -1,9 +0,0 @@
-JSoop.define('Spine.view.ViewManager', {
-    extend: 'Spine.collection.Dictionary',
-
-    singleton: true,
-
-    getKey: function (item) {
-        return item.getId();
-    }
-});
